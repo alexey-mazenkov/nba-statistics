d3565598cf5355a90b703da6e7a3732120bbb2b2
alexey-mazenkov <mazenkovof@gmail.com>
2019-11-26 13:22:34 +0700

    Parse player statistics.

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 # kek
+
+import urllib.request as ur
+
+url = 'http://www.nfl.com/player/brycepetty/2552369/profile'
+
+
+f = ur.urlopen(url)
+s = f.read()
+text = str(s)
+part_name = text.find('TOTAL')
+
+name = text[text.find('>', part_name) + 1:text.find('</tr>', part_name)]
+name = name.replace('n', '')
+name = name.replace('t', '')
+name = name.replace('d', '')
+name = name.replace('<', '')
+name = name.replace('>', '')
+name = name.replace('/', ' ')
+name = name.replace('\\', '')
+name = name.replace(',', '')
+name = name.split()
+
+COMP = name[0]
+ATT = name[1]
+YDS = name[3]
+TD = name[5]
+INT = name[6]
+print(COMP, ATT, YDS, TD, INT)
