de856e3cb4e026db3e16f92c8f1e9ade8f27b01b
KKravtsov14 <kostya.kravtsov.2001@mail.ru>
2019-12-02 13:38:09 +0700

    fix some bugs

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# kek
+#
 
 import urllib.request as ur
 
@@ -37,36 +37,34 @@ def stats(stats):
     d = 2.375 - (INT / ATT * 25)
     PR = format((((a + b + c + d) / 6) * 100), '.2f')
 
-    print(COMP, ATT, YDS, TD, INT, PR)
+    return ATT, COMP, YDS, TD, INT, PR
 
 
 def output(name, ATT, COMP, YDS, TD, INT, PR):
     all_stats = [name, ATT, COMP, YDS, TD, INT, PR]
-    outp = open('output.txt', 'a')
-    for i in range(len(all_stats)):
-        if i == 0:
-            outp.write('Player name: ')
-        elif i == 1:
-            outp.write('Number of passing attempts: ')
-        elif i == 2:
-            outp.write('Number of completions: ')
-        elif i == 3:
-            outp.write('Passing yards: ')
-        elif i == 4:
-            outp.write('Touchdown passes: ')
-        elif i == 5:
-            outp.write('Interceptions: ')
-        else:
-            outp.write('Passer rating: ')
-        outp.write(str(all_stats[i]) + '\n')
-
-    outp.close()
+    with open('output.txt', 'a') as outp:
+        for i in range(len(all_stats)):
+            if i == 0:
+                outp.write('Player name: ')
+            elif i == 1:
+                outp.write('Number of passing attempts: ')
+            elif i == 2:
+                outp.write('Number of completions: ')
+            elif i == 3:
+                outp.write('Passing yards: ')
+            elif i == 4:
+                outp.write('Touchdown passes: ')
+            elif i == 5:
+                outp.write('Interceptions: ')
+            else:
+                outp.write('Passer rating: ')
+            outp.write(str(all_stats[i]) + '\n')
 
 
 def main():
 
     print(parse_name(get_page(address)))
-    stats(parse_stat(get_page(address)))
+    print(stats(parse_stat(get_page(address))))
 
 
 main()
